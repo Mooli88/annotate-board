6a3e11e183f77c82d7364fee00d5a4514e03f8eb
mooli <mooli@universlabs.co.uk>
2020-12-04 13:19:50 +0000

    feature: add noteEditor

---
+++ src/components/NoteEditor/NoteEditor.test.tsx
@@ -0,0 +1,69 @@
+import {fireEvent} from '@testing-library/dom';
+import {render} from '@testing-library/react';
+import React from 'react';
+import NoteEditor from './NoteEditor';
+
+test('hidden by default', () => {
+  const {getByTestId} = render(<NoteEditor visible={false} onSubmit={() => {}} />);
+  const testEditorEl = getByTestId('note-editor');
+
+  expect(testEditorEl).not.toBeVisible();
+});
+
+test('show if visible set to true', () => {
+  const {getByTestId, rerender} = render(<NoteEditor visible onSubmit={() => {}} />);
+  const testEditorEl = getByTestId('note-editor');
+
+  expect(testEditorEl).toBeVisible();
+
+  rerender(<NoteEditor visible={false} onSubmit={() => {}} />);
+
+  expect(testEditorEl).not.toBeVisible();
+});
+
+test('init with default value', () => {
+  const {getByDisplayValue, getByPlaceholderText, unmount} = render(
+    <NoteEditor visible onSubmit={() => {}} />
+  );
+  const value = 'init with value';
+  let inputEl = getByPlaceholderText('Enter annotation note') as HTMLInputElement;
+
+  expect(inputEl).toBeInstanceOf(HTMLInputElement);
+  expect(inputEl).not.toHaveValue();
+
+  unmount();
+
+  render(<NoteEditor visible onSubmit={() => {}} value={value} />);
+
+  inputEl = getByDisplayValue(value) as HTMLInputElement;
+
+  expect(inputEl).toHaveValue(value);
+  expect(inputEl.value).toBe(value);
+});
+
+test('pass value on submit', () => {
+  const value = 'value to pass';
+  const onSubmit = jest.fn();
+  const {getByText} = render(<NoteEditor visible {...{onSubmit, value}} />);
+
+  const buttonEl = getByText('submit');
+  fireEvent.click(buttonEl);
+  expect(onSubmit).toHaveBeenCalledTimes(1);
+  expect(onSubmit).toBeCalledWith(value);
+});
+
+test('clear value on submit', () => {
+  const value = 'to be cleared';
+  const {getByText, getByPlaceholderText} = render(
+    <NoteEditor visible onSubmit={() => {}} value={value} />
+  );
+
+  const inputEl = getByPlaceholderText('Enter annotation note') as HTMLInputElement;
+  const buttonEl = getByText('submit');
+
+  fireEvent.click(buttonEl);
+
+  expect(inputEl).not.toHaveDisplayValue(value);
+  expect(inputEl).not.toHaveValue();
+  expect(inputEl.value).toBe('');
+});
---
+++ src/components/NoteEditor/NoteEditor.tsx
@@ -0,0 +1,35 @@
+import React, {useRef} from 'react';
+
+interface Props {
+  visible: boolean;
+  onSubmit: (note: string) => void;
+  position?: 'top' | 'bottom';
+  value?: string;
+}
+
+const NoteEditor = ({visible, onSubmit, value = '', position = 'top'}: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const submit = () => {
+    onSubmit(inputRef.current!.value);
+    inputRef.current!.value = '';
+  };
+  return (
+    <div data-testid="note-editor" style={{display: visible ? 'block ' : 'none'}}>
+      <div>
+        <label htmlFor="noteEditor">Note</label>
+        <input
+          id="noteEditor"
+          type="text"
+          placeholder="Enter annotation note"
+          defaultValue={value}
+          ref={inputRef}
+        />
+        <button onClick={submit} type="submit">
+          submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NoteEditor;
